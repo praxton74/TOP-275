82947de67c73b6d8198389ffc8b5e3a6760a73ed
Parth <94388365+praxton74@users.noreply.github.com>
2023-06-27 22:35:27 +0530

    MAP

---
+++ 27-06-23/Two Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int B=target;
+        unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++)
+    {
+        if(mp.find(B-nums[i])==mp.end())
+        {
+            mp[nums[i]]=i;
+        }
+        else
+        {
+            return {i,mp[B-nums[i]]};
+        }
+    }
+    return {0,0};  
+    }
+};
