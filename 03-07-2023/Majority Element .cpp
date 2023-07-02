89504ccbc4c2720ee38c1d41543b859341203d1c
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-03 02:30:26 +0530

    Keep it Simple MAP

---
+++ 03-07-2023/Majority Element .cpp
@@ -0,0 +1,22 @@
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        // your code here
+        map<int,int>mp;
+        for(int i=0 ; i<size ; i++)
+        {
+            mp[a[i]]++;
+            if(mp[a[i]]>size/2)
+            {
+              return a[i];
+            }
+        }
+        return -1;
+      }
+};
