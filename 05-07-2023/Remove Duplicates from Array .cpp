8b5517cbc0108e1df96a2c3918e7f39c369eac11
Parth <94388365+praxton74@users.noreply.github.com>
2023-07-05 14:56:18 +0530

    Array

--- 05-07-2023/Remove Duplicates from Array .cpp
+++ 05-07-2023/Remove Duplicates from Array .cpp
@@ -1,26 +1,19 @@
 class Solution {
-public:
+    public:
     int removeDuplicates(vector<int>& nums)
     {
-    vector<int>ans;
-	int n=nums.size();
-	if(n==0)
-	{
-		return 0;
-	}
-	if(n==1)
-	{
-		return 1;
-	}
-	for(int i=0;i<nums.size()-1;i++)
-	{
-		if(nums[i]!=nums[i+1])
-		{
-			ans.push_back(nums[i]);
-		}
-	}
-	ans.push_back(nums[n-1]);
-	int s=ans.size();
-	return s;    
+        int i=1,j=1,cnt=1;
+        int n=nums.size();
+        while(j<n){
+            if(nums[j]!=nums[j-1])
+	    { 
+                nums[i]=nums[j];
+                i++;
+                cnt++;
+                    
+            }
+            j++;
+        }
+        return cnt;
     }
 };
